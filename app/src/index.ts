0d22efdaf4e86d394bfcf2bbb1522ea3949f4e38
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-06-03 17:22:34 +1200

    cleanup main app file

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,11 +1,20 @@
-import ExtensionTest from './classes/ExtensionTest';
+import ExtensionLogger from './classes/ExtensionHelper';
 
 function main() {
     Neutralino.init();
 
-    console.log('hello world');
+    ExtensionLogger.add('js.neutralino.sample.my_nodejs_extension');
 
-    (window as unknown as { extTest: ExtensionTest }).extTest = new ExtensionTest();
+    ExtensionLogger.addEvent('js.neutralino.sample.my_nodejs_extension', 'testEvent', ({ detail }) =>
+        console.log(detail[0]),
+    );
+
+    (window as unknown as { extTest: () => void }).extTest = () => {
+        const value = Math.random().toString(36).substring(2);
+        console.log(`Calling extension with input: ${value}`);
+
+        Neutralino.extensions.dispatch('js.neutralino.sample.my_nodejs_extension', 'testEvent', value);
+    };
 }
 
 main();

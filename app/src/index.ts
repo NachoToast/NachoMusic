05357411573b35ac760fcc4f4c50c4b1c4b750e9
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-06-09 22:05:30 +1200

    main app file tweaks

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,8 +1,8 @@
-import ContentManager from './classes/ContentManager';
 import ExtensionLogger, { WatchEventLevels } from './classes/ExtensionHelper';
-import SettingsHelper from './classes/SettingsHelper';
+import Settings from './classes/Settings';
+import SongManager from './classes/SongManager';
 
-async function main() {
+function main() {
     Neutralino.init();
 
     ExtensionLogger.add(
@@ -11,38 +11,27 @@ async function main() {
         'sampleExt',
     );
 
-    await ContentManager.getInstance();
-    const s = await SettingsHelper.getInstance();
-    console.log(s.defaultSettings);
+    ExtensionLogger.add('js.nachotoast.youtubesearch', 'ALL', 'ytsr');
 
-    ExtensionLogger.addEvent('sampleExt', 'testEvent', ({ detail }) => console.log(detail[0]));
+    Settings.getInstance();
+    SongManager.getInstance();
+
+    ExtensionLogger.addEvent('ytsr', 'testEvent', ({ detail }) => console.log(detail[0]));
 
     (window as unknown as { extTest: () => void }).extTest = () => {
         const value = Math.random().toString(36).substring(2);
         console.log(`Calling extension with input: ${value}`);
 
-        Neutralino.extensions.dispatch('js.neutralino.sample.my_nodejs_extension', 'testEvent', value);
+        Neutralino.extensions.dispatch('js.nachotoast.youtubesearch', 'testEvent', value);
     };
 
-    // contentManager.addSong('C:\\Users\\fuzzy\\Music\\NachoMusic\\Alan Walker - Faded.mp4');
-    // const v = document.createElement('video');
-    // v.controls = true;
-    // const s = document.createElement('source');
-    // s.type = 'video/mp4';
-    // s.src = 'data/Alan Walker - Faded.mp4';
-    // v.muted = true;
-    // v.appendChild(s);
-    // v.oncanplaythrough = () => {
-    //     console.log('playing');
-    //     v.play();
-    //     v.muted = false;
-    // };
-    // document.body.appendChild(v);
-    // v.autoplay = true;
-    // v.onload = () => {
-    // v.autoplay = true;
-    // v.play();
-    // };
+    document.getElementById('songSearchInputGoButton')!.onclick = () => {
+        const queryString = (document.getElementById('songSearchInput')! as HTMLInputElement).value;
+
+        console.log('querying with value', queryString);
+
+        Neutralino.events.dispatch('youtubeSearchQuery', queryString);
+    };
 }
 
 main();

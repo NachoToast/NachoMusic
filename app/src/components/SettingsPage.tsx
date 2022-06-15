d990d900517538cac6a7cd4db1d057b2d79c655e
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-06-16 02:58:25 +1200

    temporary settings page

--- app/src/components/SettingsPage.tsx
+++ app/src/components/SettingsPage.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { CircularProgress, Container, Paper, Stack, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { getSettings } from '../redux/slices/settingsSlice';
 
 const SettingsPage = () => {
-    return <div>settings</div>;
+    const settings = useSelector(getSettings);
+
+    if (settings === undefined) {
+        return (
+            <Stack justifyContent="center" alignItems="center" spacing={1}>
+                <CircularProgress size={60} />
+                <Typography variant="h5">Loading Settings...</Typography>
+            </Stack>
+        );
+    }
+
+    return (
+        <Container maxWidth="md" sx={{ pt: 3 }}>
+            <Paper sx={{ whiteSpace: 'pre-wrap', p: 1 }}>{JSON.stringify(settings, undefined, 4)}</Paper>
+        </Container>
+    );
 };
 
 export default SettingsPage;

43b7cb53a4c10c38892aaf435e2b9be41829afcd
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-06-16 13:16:03 +1200

    utilize new types for extensions

--- extensions/src/modules/YouTubeSearcher.ts
+++ extensions/src/modules/YouTubeSearcher.ts
@@ -1,12 +1,35 @@
-import ytsr from 'ytsr';
+import ytsr, { Video } from 'ytsr';
+import { SearchResponse } from '../../../shared/YouTube';
 import BasicSocket from '../helpers/BasicSocket';
 
 function YouTubeSearcher(socket: BasicSocket) {
     socket.on('youtubeSearchQuery', async ({ queryString, limit }) => {
         try {
-            const results = await ytsr(queryString, { limit });
+            const allResults = await ytsr(queryString, { limit });
 
-            socket.send('youtubeSearchResult', results);
+            const outputResults: SearchResponse = [];
+
+            for (let i = 0, len = allResults.items.length; i < len; i++) {
+                if (allResults.items[i].type === 'video') {
+                    const video = allResults.items[i] as Video;
+                    if (video.isLive === false && video.isUpcoming === false) {
+                        outputResults.push({
+                            title: video.title,
+                            id: video.id,
+                            url: video.url,
+                            bestThumbnail: video.bestThumbnail,
+                            badges: video.badges,
+                            author: video.author,
+                            description: video.description,
+                            views: video.views,
+                            duration: video.duration,
+                            uploadedAt: video.uploadedAt,
+                        });
+                    }
+                }
+            }
+
+            socket.send('youtubeSearchResult', outputResults);
         } catch (error) {
             socket.handleError(error instanceof Error ? error : new Error(`Error getting results for ${queryString}`));
         }

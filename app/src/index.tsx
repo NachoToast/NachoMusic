01493602e382238fec48e034b0fc7d99d8edc096
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-12-02 20:41:36 +1300

    extension logging

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -9,3 +9,17 @@ root.render(
         <App />
     </React.StrictMode>,
 );
+
+Neutralino.events.on('extensionLog', ({ detail: { id, message } }) => {
+    console.log(`[${id}]`, message);
+});
+
+Neutralino.events.on('extensionError', ({ detail: error }) => {
+    console.log(error.id, error.name, error.message, error.stack);
+});
+
+Neutralino.events.on('extensionReady', ({ detail: id }) => {
+    console.log(`${id} ready`);
+});
+
+Neutralino.init();
